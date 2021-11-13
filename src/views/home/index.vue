e99e0cd1247d71fe6725eb6f074866ae0cd9063e
范力 <798347235@qq.com>
2021-11-13 17:42:52 +0800

    fix: bug

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -9,7 +9,7 @@
         <el-button type="primary" size="small" @click="btn">按钮1</el-button>
         <el-button type="primary" size="small" @click="btn">按钮2</el-button>
       </el-main>
-      <el-footer>Footer000000000sadhaioufhgdksajhdjklasgfkulahsbdlkgh</el-footer>
+      <el-footer style="color: #fff">王老板，能请我吃个饭不</el-footer>
     </el-container>
   </div>
 </template>
